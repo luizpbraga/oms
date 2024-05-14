46dcb8f2d396433303957d9fe4e83918b3d22a28
Luiz Alberto Portilho Braga <portilho.braga@gmail.com>
2024-05-14 13:35:02 -0300

    gateway: handle gRPC

--- gateway/handler.go
+++ gateway/handler.go
@@ -1,18 +1,42 @@
 package main
 
-import "net/http"
+import (
+	"log"
+	"net/http"
+
+	"github.com/luizpbraga/common"
+	pb "github.com/luizpbraga/common/api"
+)
 
 type handler struct {
 	// todo: gateway dependencies
+	client pb.OrderServiceClient
 }
 
-func NewHandler() *handler {
-	return &handler{}
+func NewHandler(client pb.OrderServiceClient) *handler {
+	return &handler{
+		client: client,
+	}
 }
 
 func (h *handler) registerRouters(mux *http.ServeMux) {
-	mux.HandleFunc("POST /api/custumers/{customerID}/orders", h.handleCreateOrder)
+	mux.HandleFunc("POST /api/custumers/{customerID}/orders", h.HandleCreateOrder)
 }
 
-func (h *handler) handleCreateOrder(w http.ResponseWriter, r *http.Request) {
+func (h *handler) HandleCreateOrder(w http.ResponseWriter, r *http.Request) {
+	cuntumerID := r.PathValue("curtomerID")
+
+	var items []*pb.ItemsWithQuantity
+
+	if err := common.ReadFromJson(r, &items); err != nil {
+		common.WriteError(w, http.StatusBadRequest, err)
+		return
+	}
+
+	orderRequest := pb.CreateOrderRequest{
+		CustumerID: cuntumerID,
+		Item:       items,
+	}
+
+	h.client.CreateOrder(r.Context(), &orderRequest)
 }
--- gateway/main.go
+++ gateway/main.go
@@ -6,17 +6,35 @@ import (
 
 	_ "github.com/joho/godotenv/autoload"
 	"github.com/luizpbraga/common"
+	pb "github.com/luizpbraga/common/api"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials/insecure"
 )
 
 func main() {
-	httpAddr := common.Getenv("HTTP_ADDR", ":3000")
+	httpAddr := common.Getenv("HTTP_ADDR", ":8080")
+	orderServiceAddr := "localhost:2000"
+
+	conn, err := grpc.Dial(
+		orderServiceAddr,
+		grpc.WithTransportCredentials(insecure.NewCredentials()),
+	)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer conn.Close()
+
+	client := pb.NewOrderServiceClient(conn)
+
+	log.Println("Dialing Order Service At ", orderServiceAddr)
+
 	mux := http.NewServeMux()
-	handler := NewHandler()
+	handler := NewHandler(client)
 	handler.registerRouters(mux)
 
-	log.Printf("Starting HTPP server at %s\n", httpAddr)
+	log.Printf("Starting HTTP server at %s\n", httpAddr)
 
 	if err := http.ListenAndServe(httpAddr, mux); err != nil {
-		log.Fatal("Failed to start the server")
+		log.Fatal("Failed to start the server: ", err.Error())
 	}
 }
